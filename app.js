84c0bde39100ed07bf862456f94632dde43de199
oussama achemlal <oussama.achemlal7@gmail.com>
2021-10-15 16:51:30 +0100

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+switch (operation){
+    case '+':
+        console.log(addition(nombre1, nombre2))
+    break;
+    case '-':
+    
+    break;
+    case '*':
+    
+    break;
+    case '/':
+
+    break;
+    case '%':
+
+    break;
+
+}
+
+element.addEventListener("click",()=>{
+    let operation = element.getAttribute("value")
+    console.log(element)
+   
+    
+})
